917f216a8d3da2c4619637c689786c5e94c131ae
Dmitry Sky <dmitry.sky@icloud.com>
2015-06-09 22:35:51 -0600

    user can create account feature test implemented and passing.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def show
+    # we create helper method because we don't just want
+    # to know what user is logged in on the show page but
+    #throughout the site
+    @user = current_user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user #user_path(@user)
+    else
+      # if user is not saved
+      # re-render :new if validations don't pass
+    end
+    #when a user creates an account they should automatically
+    #be logged in, so we keep track of the user_id in a session
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
---
+++ spec/features/user_creation_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'User Creates Account', type: :feature do
+  scenario 'a user can be created' do
+    visit new_user_path
+    
+    fill_in 'Username', with: 'samski'
+    fill_in 'Password', with: 'rules'
+    click_on 'Create Account'
+
+    expect(page).to have_content('samski')
+  end
+end
+
